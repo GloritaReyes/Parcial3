61d825909c63a0e94e211e0e357e9641888a4ee2
GloritaReyes <reyesgloria654@gmail.com>
2023-10-30 12:33:05 -0500

    proyecto

--- app/src/main/java/com/example/parcial3/HomeFragment.java
+++ app/src/main/java/com/example/parcial3/HomeFragment.java
@@ -4,47 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
-    public HomeFragment() {
-    }
-
+     ImageButton imageButton;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_home, container, false);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_home, container, false);
 
-    public void Escritorio1(View view) {
-    }
 
-    public void Escritorio2(View view) {
+        return  view;
     }
 
 
-    public void Escritorio3(View view) {
-    }
 
-    public void Escritorio4(View view) {
-    }
-
-    public void Escritorio5(View view) {
-    }
-
-    public void Escritorio6(View view) {
-    }
-
-    public void Escritorio7(View view) {
-    }
-
-    public void Escritorio8(View view) {
-    }
-
-    public void Escritorio9(View view) {
-    }
-
-    public void Escritorio10(View view) {
-    }
 }
