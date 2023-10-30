b2a28f7cad2caf4ec7e814e2ed1dc6be1c76047b
GloritaReyes <reyesgloria654@gmail.com>
2023-10-30 18:21:18 -0500

    Hola mundo

--- app/src/main/java/com/example/parcial3/HomeFragment.java
+++ app/src/main/java/com/example/parcial3/HomeFragment.java
@@ -5,22 +5,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
-     ImageButton imageButton;
+    private View v;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_home, container, false);
+        v = inflater.inflate(R.layout.activity_home, container, false);
 
-
-        return  view;
+        configureImageButton();
+        return v;
     }
 
+    private void configureImageButton() {
+        ImageButton btn = (ImageButton) v.findViewById(R.id.boton1);
 
+        btn.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "You Clicked the button!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/example/parcial3/escritorio1.java
@@ -0,0 +1,14 @@
+package com.example.parcial3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class escritorio1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escritorio1);
+    }
+}
